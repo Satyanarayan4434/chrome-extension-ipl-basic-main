d8d6384832e49ed72e3b5b15448ed151e4e6dd75
Satyanarayan Sen <60086933+Satyanarayan4434@users.noreply.github.com>
2023-05-22 14:22:07 +0530

    final commit

--- script.js
+++ script.js
@@ -1,19 +1,17 @@
+document.getElementById("heading1").setAttribute("style","font-family: 'Ysabeau', sans-serif;font-size: 1.5rem;text-align: center;color: whitesmoke;background-color: rgba(0, 0, 0, 0.315);border-radius: 25px;");
+document.getElementById("IPLmatches").setAttribute("style","color:white; font-size: 1rem;  font-family: 'Prompt', sans-serif;");
 async function getMatchData() {
   return await fetch(
-    "https://api.cricapi.com/v1/currentMatches?apikey=c2b6a7c7-e0df-4433-bb3b-681829136d26&offset=0"
+    "https://api.cricapi.com/v1/currentMatches?apikey=67fd01a2-3540-4369-b0a0-4f4d1c079e90&offset=0"
   )
     .then((data) => data.json())
     .then((data) => {
       if (data.status != "success") return;
-
-      const matchesList = data.data;
-      
+      const matchesList = data.data;     
       if (!matchesList) return [];
       //add your api key from cricketdata.org
       const relevantData = matchesList.filter(iplMatchOnly => iplMatchOnly.series_id == "c75f8952-74d4-416f-b7b4-7da4b4e3ae6e").map((match) => `${match.name}, ${match.status}`);
-      console.log({ relevantData });
-      document.getElementById("IPLmatches").innerHTML = relevantData.map(matchDetails => `<li>${matchDetails}</li>`).join('');
-    
+       document.getElementById("IPLmatches").innerHTML = relevantData.map(matchDetails => `<li>${matchDetails}.</li>`).join('');
     })
   .catch(e => console.log(e));
 }
