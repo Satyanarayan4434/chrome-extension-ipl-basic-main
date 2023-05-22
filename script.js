ed0e34cb0514c95e51d525798aca7f643b5bc4d0
Satyanarayan Sen <60086933+Satyanarayan4434@users.noreply.github.com>
2023-05-22 10:20:50 +0530

    Data fetches from API

--- script.js
+++ script.js
@@ -1,25 +1,21 @@
 async function getMatchData() {
   return await fetch(
-    "https://api.cricapi.com/v1/currentMatches?apikey=aa1c2440-a6f3-488e-991e-64fb0eab86b7&offset=0"
+    "https://api.cricapi.com/v1/currentMatches?apikey=c2b6a7c7-e0df-4433-bb3b-681829136d26&offset=0"
   )
     .then((data) => data.json())
     .then((data) => {
       if (data.status != "success") return;
 
       const matchesList = data.data;
-
-      if (!matchesList) return [];  
-
+      
+      if (!matchesList) return [];
       //add your api key from cricketdata.org
-      const relevantData = matchesList.filter((match) => match.series_id == "{c75f8952-74d4-416f-b7b4-7da4b4e3ae6e}").map((match) => `${match.name}, ${match.status}`);
-
+      const relevantData = matchesList.filter(iplMatchOnly => iplMatchOnly.series_id == "c75f8952-74d4-416f-b7b4-7da4b4e3ae6e").map((match) => `${match.name}, ${match.status}`);
       console.log({ relevantData });
-
-    //   document.getElementById("matches").innerHTML = relevantData.map((match) => `<li>${match} </li>`).join("");
-
-    //   return relevantData;
+      document.getElementById("IPLmatches").innerHTML = relevantData.map(matchDetails => `<li>${matchDetails}</li>`).join('');
+    
     })
-    .catch((e) => console.log(e));
+  .catch(e => console.log(e));
 }
 
 getMatchData();
