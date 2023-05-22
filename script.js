4bfd5db460fc8fab5b886b6f98fa596b412dd3a3
Satyanarayan Sen <60086933+Satyanarayan4434@users.noreply.github.com>
2023-05-22 09:17:12 +0530

    insert my api key

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
 async function getMatchData() {
+  return await fetch(
+    "https://api.cricapi.com/v1/currentMatches?apikey=aa1c2440-a6f3-488e-991e-64fb0eab86b7&offset=0"
+  )
+    .then((data) => data.json())
+    .then((data) => {
+      if (data.status != "success") return;
 
-    return await fetch("https://api.cricapi.com/v1/currentMatches?apikey=bef4b6e4-0fbb-4e11-9410-6e16e2ca5ab1&offset=0")
-        .then(data => data.json())
-        .then(data => {
-            if (data.status != "success")return;
+      const matchesList = data.data;
 
-            const matchesList = data.data;
+      if (!matchesList) return [];  
 
-            if(!matchesList)return [];
-            
-            //add your api key from cricketdata.org
-            const relevantData = matchesList.filter(match => match.series_id == "{your_api_key}").map(match => `${match.name}, ${match.status}`);
+      //add your api key from cricketdata.org
+      const relevantData = matchesList.filter((match) => match.series_id == "{c75f8952-74d4-416f-b7b4-7da4b4e3ae6e}").map((match) => `${match.name}, ${match.status}`);
 
-            console.log({relevantData});
+      console.log({ relevantData });
 
-            document.getElementById("matches").innerHTML = relevantData.map(match => `<li>${match} </li>`).join('');
+      document.getElementById("matches").innerHTML = relevantData.map((match) => `<li>${match} </li>`).join("");
 
-            return relevantData;
-
-        })
-        .catch(e => console.log(e));
+      return relevantData;
+    })
+    .catch((e) => console.log(e));
 }
 
 getMatchData();
